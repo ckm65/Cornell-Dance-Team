37df2b15b9ed93daeaa4e85ac0ad4baebca6d98f
Yifei Zheng <yz362@cornell.edu>
2019-12-03 17:11:57 -0500

    gallery js

--- scripts/interactivity.js
+++ scripts/interactivity.js
@@ -1,3 +1,32 @@
 $(document).ready(function(){
-  
+  var slides= [
+    "images/showcase1.jpg",
+    "images/showcase2.jpg",
+    "images/showcase3.jpg",
+    "images/showcase4.jpg",
+    "images/showcase5.jpg",
+    "images/showcase6.jpg",
+  ];
+
+  var index = 0;
+
+  $("#slide_left").on("click", function() {
+    if (index == 0){
+      index = 5;
+      $("#slideshowPhoto").attr("src",slides[index]);
+    }else{
+      index = index - 1;
+      $("#slideshowPhoto").attr("src",slides[index]);
+    }
+  })
+
+  $("#slide_right").on("click", function() {
+    if (index == 5){
+      index = 0;
+      $("#slideshowPhoto").attr("src",slides[index]);
+    }else{
+      index = index + 1;
+      $("#slideshowPhoto").attr("src",slides[index]);
+    }
+  })
 })
